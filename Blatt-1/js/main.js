1d2004d43762917cfba7adcd4f584bd9db1fa42a
Na22eR <NasserYacineACCS@gmail.com>
2023-05-21 21:20:00 +0200

    Aufgabe 2 abgeschlossen

--- Blatt-1/js/main.js
+++ Blatt-1/js/main.js
@@ -1,21 +1,51 @@
 
-console.log("Init complete")
-
-
-function test(){
-
-    document.getElementById("halloLink").addEventListener("click", function(){
-        console.log("Link geklickt");
-        //e.preventDefault();
-
-    } );
+function init(){
+    addListenerCanvas()
+    addMoveListenerCanvas()
+    addClickListenerCanvas()
+    console.log("Init complete")
 }
 
 
-function test2(){
+function addListenerCanvas(){
 
-    document.getElementById("halloLink").addEventListener("contextmenu", function(){
-        console.log("Link geklickt"); } );
+    document.getElementById("canvas").addEventListener("contextmenu", function(e){
+        console.log("Event: Canvas right clicked");
+        e.preventDefault();
+    } );
+
+
+}
+
+function addMoveListenerCanvas(){
+
+    document.getElementById("canvas").addEventListener("mousemove", function(e){
+        var x = e.clientX - 8
+        var y = e.clientY - 8
+        document.getElementById("coordinates").textContent = "Coordinates: " + x + ", " + y;
+        console.log("Event: Mouse moved");
+        //console.log("Coordinates: " + x + ", " + y);
+        e.preventDefault();
+    } );
+
+
+}
+
+
+function addClickListenerCanvas(){
+
+    document.getElementById("canvas").addEventListener("click", function(e){
+        var x = e.clientX - 8
+        var y = e.clientY - 8
+
+        var list = document.getElementById("CordList");
+        var listItem = document.createElement("li");
+        listItem.textContent = "X-Pos: " + x + ", Y-Pos: " + y
+        list.appendChild(listItem)
+
+        console.log("Event: Canvas left clicked");
+        e.preventDefault();
+    } );
 
 
 }
